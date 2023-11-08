47b73e2bf425d02c484c3a9246c70e11092eca86
Asif Saif Ali <asif263848@gmail.com>
2023-11-08 22:24:16 +0530

    Chnges in index.js files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express"
+import routes from './routes/index.js'
 // import  PORT  from "./config/serverConfig.js"
 import Connection from "./config/database.js";
 import NoteService from "./services/Notes-service.js";
 import bodyParser   from "body-parser";
 const app = express();
+const ApiRoute = routes;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:true}));
+app.use('/Notes/api',ApiRoute);
 
 const PORT = 3000
 app.listen(3000,async()=>{ 
